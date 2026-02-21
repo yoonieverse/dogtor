10c5a487e6eef37f2575880ddc6b6e7f3d040d6a
chaiyun <yoonsuhr@gmail.com>
2026-02-21 15:45:20 -0600

    Created basic react app with express node backend

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # dogtor
+
+
+npm create vite@latest frontend
+
+mkdir frontend
+cd frontend
+npm install
+npm run dev
+
+mkdir backend
+cd backend 
+npm init -y
+npm install express
+npm install nodemon cors
+(since downloaded nodemon you run: )
+npm run dev
---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Backend is running!');
+});
+
+app.listen(5000, () => {
+  console.log('Server running on port 5000');
+});
